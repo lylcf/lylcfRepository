682e7d04a24478fe8420de5668de2a88cddc7a47
lylcf <lan@lanoudeMac-mini-99.local>
2016-05-25 15:50:44 +0800

    新建了一个空项目

---
+++ testProject/testProject/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  testProject
+//
+//  Created by lanou on 16/5/25.
+//  Copyright © 2016年 黄梦楚. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
